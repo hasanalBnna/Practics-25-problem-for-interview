c2b55e65550b1b29da56474d62a441f406c947e7
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-08-11 23:48:28 +0600

    fourth commit

---
+++ src/Reverse_an_Array_4/Main.java
@@ -0,0 +1,13 @@
+
+package Reverse_an_Array_4;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int[] ara = new int[]{1, 2, 30, 4, 50};
+    for(int i = ara.length - 1;i>=0;i--)
+    System.out.print(ara[i] +" ");
+    System.out.println("");
+    }
+    
+}
