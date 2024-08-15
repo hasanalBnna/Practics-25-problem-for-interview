14a2fc1bf1260f403f339b6d050bb3151bce2852
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-08-15 22:59:25 +0600

    9th commit

--- src/Reverse_an_Array_4/ReverseString.java
+++ src/Reverse_an_Array_4/ReverseString.java
@@ -1,17 +1,21 @@
 
 package Reverse_an_Array_4;
 
+import java.util.Scanner;
+
 
 public class ReverseString {
 
     public static void main(String[] args) {
-    String str= "abcd";
-    String show = " " ;
-    for(int i=str.length() - 1; i>=0 ;i--){
-        //System.out.println(str.charAt(i));
-        show += str.charAt(i);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str= sc.nextLine();
+        String show = " " ;
+        for(int i=str.length() - 1; i>=0 ;i--){
+            //System.out.println(str.charAt(i));
+             show += str.charAt(i);
     }
-    System.out.println("Reverse this String = "+show);
+    System.out.println("Reverse this String: "+show);
     }
     
 }
