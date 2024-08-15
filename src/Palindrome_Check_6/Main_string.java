5470cb0c2ac731019975db4e224dc650ff8fe151
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-08-15 23:48:28 +0600

    10th commit

--- src/Palindrome_Check_6/Main_string.java
+++ src/Palindrome_Check_6/Main_string.java
@@ -1,9 +1,24 @@
 
 package Palindrome_Check_6;
 
+import java.util.Scanner;
+
 public class Main_string {
 
     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str= sc.nextLine();
+        String show = "" ;
+        for(int i=str.length() - 1; i>=0 ;i--){
+            show = show + str.charAt(i);
+    }
+        if(str.equals(show)){
+            System.out.println("String is a palindrom");
+        }
+        else
+            System.out.println("String is not a palindrom");
     
     }
     
